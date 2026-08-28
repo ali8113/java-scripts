96d989bc46ea2b503601c5b14bd8cc3a13dc4f6b
Ali Raza <ar4197711@gmail.com>
2026-08-28 09:58:00 +0000

    conservation od datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const account_Id = 144567
+let accountEmail = "example@testing.com"
+var accountPassword = "123455"
+accountCity = "USA"
+
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "232323"
+accountCity = "UK"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// alert( 3 + 3)
+
+console.log(3 + 3)
+console.log("Hello")
+
+
+
+let name = "ALex"
+let age = 24
+let isLoggedIn = false
+let state;
+
+console.log(typeof null);
